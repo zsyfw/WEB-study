26bb56cf4c7f165f8cb1b821369e04983aa6be72
fw <zsyfw20180429@163.com>
2023-09-07 14:34:14 +0800

    version 1.2

--- springboot-study/src/main/java/webstudy/controller/helloworld.java
+++ springboot-study/src/main/java/webstudy/controller/helloworld.java
@@ -3,6 +3,7 @@ package webstudy.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import webstudy.Class.User;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -17,9 +18,20 @@ public class helloworld {
     public String SelectForId(int id){
         return "id是"+id;
     }
-    @RequestMapping("/SelectForId_2")
+    @RequestMapping("/SelectForId_2")                  //此时会根据name来接收参数,但此参数必须存在,除非设置required = false
     public String SelectForId_2(@RequestParam(name = "name") int id){
-        return "id是"+id;                      //此时会根据name来接收参数
+        return "id是"+id;
     }
+    @RequestMapping("/UserInfo")
+    public String UserInfo(User user){
+        System.out.println(user);
+        return user.toString();
+    }
+    @RequestMapping("/UserAddressInfo")
+    public String UserAddressInfo(User user){
+        System.out.println(user);
+        return user.toString();
+    }
+
 
 }
